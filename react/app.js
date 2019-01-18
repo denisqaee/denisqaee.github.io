6e4baaad4300d18388e47c254c6d4862f0dc2cf8
Denis <42413129+denisqaee@users.noreply.github.com>
2019-01-18 17:50:26 +0300

    react-app

--- react/app.js
+++
@@ -1,6 +0,0 @@
-var image = React.createElement('img', { src: 'img/react.png' });
-var title = React.createElement('h1', null, 'Привет, React!');
-var subtitle = React.createElement('p', null, 'Библиотека для создания пользовательских элементов');
-var container = React.createElement('div', {className: 'container'}, image, title, subtitle); 
-
-ReactDOM.render(container, document.getElementById('root'));
