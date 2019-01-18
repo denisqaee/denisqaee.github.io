c11be3b7d57c12b81c565e8edf1a8cc2b1e5ba7f
Denis <42413129+denisqaee@users.noreply.github.com>
2019-01-18 14:43:32 +0300

    react

--- react/app.jsx
+++ react/app.jsx
@@ -1,8 +1,11 @@
-var jsx =
-<div className="containter">
-  <img src="img/react.png" />
-  <h1>React</h1>
-  <p>Библиотека для создания пользовательских интерфейсов</p>
-</div>;
+function Hero() {
+  return (
+    <div className="containter">
+      <img src="img/react.png" />
+      <h1>React</h1>
+      <p>Библиотека для создания пользовательских интерфейсов</p>
+    </div>
+  );
+}
 
-ReactDOM.render(jsx, document.getElementById('root'));
+ReactDOM.render(Hero, document.getElementById('root'));
