84373875ac3e75f1cf0467b36d057db89a1d877b
Denis <42413129+denisqaee@users.noreply.github.com>
2019-01-18 14:33:43 +0300

    react

---
+++ react/app.jsx
@@ -0,0 +1,8 @@
+var jsx =
+<div className="containter">
+  <img src="img/react.png" />
+  <h1>React</h1>
+  <p>Библиотека для создания пользовательских интерфейсов</p>
+</div>;
+
+ReactDOM.render(jsx, document.getElementById('root'));
