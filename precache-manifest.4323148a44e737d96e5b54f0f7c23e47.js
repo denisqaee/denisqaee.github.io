901f2bca99d8e7d1a5827c01eb6c0ef4791bfe66
Denis <42413129+denisqaee@users.noreply.github.com>
2019-01-18 23:24:42 +0300

    Updates

---
+++ precache-manifest.4323148a44e737d96e5b54f0f7c23e47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ec46125784e0b1f6cca6",
+    "url": "/react-app/static/js/runtime~main.ec461257.js"
+  },
+  {
+    "revision": "6e40e322379cb698d43e",
+    "url": "/react-app/static/js/main.6e40e322.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-app/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "6e40e322379cb698d43e",
+    "url": "/react-app/static/css/main.ba41973d.chunk.css"
+  },
+  {
+    "revision": "fd31bb137ebf6de8c2895dcfad484d81",
+    "url": "/react-app/index.html"
+  }
+];
